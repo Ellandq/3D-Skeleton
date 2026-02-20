a46551068451968b42b1d27a3641c80107425777
Ellandq <waselland@gmail.com>
2026-02-20 09:10:22 +0100

    Created ManagerBase

---
+++ Assets/Scripts/Managers/ManagerBase.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    public abstract class ManagerBase<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance)
+                {
+                    return _instance;
+                }
+
+                _instance = FindFirstObjectByType<T>();
+
+                if (!_instance)
+                {
+                    Debug.LogError($"No instance of type: {typeof(T)}");
+                }
+                
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            _instance = this as T;
+        }
+
+        private void OnDestroy()
+        {
+            _instance = null;
+        }
+    }
+}
