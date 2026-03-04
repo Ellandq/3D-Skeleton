c863f4c82a0656e11139a2b522c3573bcd3de242
Ellandq <waselland@gmail.com>
2026-03-04 16:55:51 +0100

    Input system configuration

---
+++ Assets/Scripts/Utils/SO/Settings/SettingSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using GameInput;
+using UnityEngine;
+
+namespace Utils.SO.Settings
+{
+    public class SettingSO : ScriptableObject
+    {
+        public List<string> settingNames;
+        public List<string> defaultValues;
+    }
+}
